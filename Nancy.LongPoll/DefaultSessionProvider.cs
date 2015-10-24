5b438ec4fe11a619e14b4d22da2dee3ce2c83e2c
Alexandr <a.slobodchikov@gmail.com>
2015-10-24 17:29:42 +0300

    Moved DefaultSessionProvider to separate file

---
+++ Nancy.LongPoll/DefaultSessionProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nancy.LongPoll
+{
+  public interface ISessionProvider
+  {
+    string SessionId { get; }
+  }
+
+  public class DefaultSessionProvider : ISessionProvider
+  {
+    public static readonly string SessionIdCookieName = "nancy_long_poll_session_id";
+    Request _Request = null;
+
+    public DefaultSessionProvider()
+    {
+    }
+
+    public DefaultSessionProvider(Request request)
+    {
+      if (request == null) throw new ArgumentNullException("request");
+
+      _Request = request;
+    }
+
+    public string SessionId
+    {
+      get
+      {
+        string sessionId = null;
+        if (!_Request.Cookies.ContainsKey(SessionIdCookieName))
+        {
+          sessionId = Guid.NewGuid().ToString("N") + Guid.NewGuid().ToString("N");
+        }
+        else
+        {
+          sessionId = _Request.Cookies[SessionIdCookieName];
+        }
+
+        return sessionId;
+      }
+    }
+  }
+}
--- Nancy.LongPoll/PollModule.cs
+++ Nancy.LongPoll/PollModule.cs
@@ -51,44 +51,4 @@ namespace Nancy.LongPoll
     }
     #endregion
   }
-
-  public interface ISessionProvider
-  {
-    string SessionId { get; }
-  }
-
-  public class DefaultSessionProvider : ISessionProvider
-  {
-    public static readonly string SessionIdCookieName = "nancy_long_poll_session_id";
-    Request _Request = null;
-
-    public DefaultSessionProvider()
-    {
-    }
-
-    public DefaultSessionProvider(Request request)
-    {
-      if (request == null) throw new ArgumentNullException("request");
-
-      _Request = request;
-    }
-
-    public string SessionId
-    {
-      get
-      {
-        string sessionId = null;
-        if (!_Request.Cookies.ContainsKey(SessionIdCookieName))
-        {
-          sessionId = Guid.NewGuid().ToString("N") + Guid.NewGuid().ToString("N");
-        }
-        else
-        {
-          sessionId = _Request.Cookies[SessionIdCookieName];
-        }
-
-        return sessionId;
-      }
-    }
-  }
 }
